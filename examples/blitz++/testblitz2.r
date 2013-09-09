42b94dc414cac12c86ad4afba39cfdf62e41849a
Florian Oswald <florian.oswald@gmail.com>
2013-09-09 12:26:01 +0100

    had to change fortranArray to FortranArray<dim>() for some reason.

--- examples/blitz++/testblitz2.r
+++ examples/blitz++/testblitz2.r
@@ -23,8 +23,8 @@ Rcpp::IntegerVector d2 = A2.attr("dim");
 // create 2 blitz cubes B1 and B2
 // Note that by default, Blitz arrays are row-major. 
 // From R and Fortran, we get column major arrays. so use option "fortranArray" to define storage order.
-blitz::Array<double,3> B1(A.begin(), shape(d1(0),d1(1),d1(2)), neverDeleteData, fortranArray);
-blitz::Array<double,3> B2(A2.begin(), shape(d2(0),d2(1),d2(2)), neverDeleteData, fortranArray);
+blitz::Array<double,3> B1(A.begin(), shape(d1(0),d1(1),d1(2)), neverDeleteData, FortranArray<3>());
+blitz::Array<double,3> B2(A2.begin(), shape(d2(0),d2(1),d2(2)), neverDeleteData, FortranArray<3>());
 
 // declare blitz dimension descriptors
 firstIndex  i;
@@ -33,16 +33,16 @@ thirdIndex  k;
 
 // create to "value functions" to store values after maximizing
 // over the third dimension of B1 and B2
-blitz::Array<double,2> V1(d1(0),d1(1),fortranArray);
-blitz::Array<double,2> V2(d1(0),d1(1),fortranArray);
+blitz::Array<double,2> V1(d1(0),d1(1),FortranArray<2>());
+blitz::Array<double,2> V2(d1(0),d1(1),FortranArray<2>());
 
 // maximize over third dim
 V1 = max(B1,k);
 V2 = max(B2,k);
 
 // map as blitz arrays
-blitz::Array<double,2> Vmax(Vout.begin(), shape(d1(0),d1(1)), neverDeleteData,fortranArray);
-blitz::Array<double,2> Dmax(Dout.begin(), shape(d2(0),d2(1)), neverDeleteData,fortranArray);
+blitz::Array<double,2> Vmax(Vout.begin(), shape(d1(0),d1(1)), neverDeleteData,FortranArray<2>());
+blitz::Array<double,2> Dmax(Dout.begin(), shape(d2(0),d2(1)), neverDeleteData,FortranArray<2>());
 
 // take discrete choice
 Vmax = where(V1 > V2, V1, V2);
@@ -58,8 +58,6 @@ Vout.attr("dim") = Rcpp::IntegerVector::create(d1(0),d1(1));
 Dout.attr("dim") = Rcpp::IntegerVector::create(d1(0),d1(1));
 Rcpp::List list = Rcpp::List::create( _["vmax"] = Vout, _["dchoice"] = Dout);
 return list;
-
-
 '
 
 A = array(1:60,c(2,5,6))
@@ -68,6 +66,8 @@ V = array(0,c(2,5))
 
 f2 = cxxfunction(signature(A_="numeric",A2_="numeric"),body=src,plugin="Rcpp",includes=inc)
 result = f2(A,A2)
+cat('blitz++ result:\n')
+print(result)
 
 # R result
 r = list()
@@ -77,4 +77,9 @@ V12 = array(c(V1,V2),c(2,5,2))
 r$vmax = apply(V12,c(1,2),max)
 r$dchoice = apply(V12,c(1,2),which.max)
 
-all.equal(result,r)
+cat('R result:\n')
+print(r)
+
+
+cat('all.equal?\n')
+print(all.equal(result,r))
